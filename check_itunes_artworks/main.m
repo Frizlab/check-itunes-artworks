3671018d5f77e7ab1a544d24941290b6248e2665
Frizlab <fload@me.com>
2012-04-05 13:27:58 +0200

    Initial Commit

---
+++ check_itunes_artworks/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  check_itunes_artworks
+//
+//  Created by François LAMBOLEY on 4/5/12.
+//  Copyright (c) 2012 Frost Land. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+	@autoreleasepool {
+	    
+	    // insert code here...
+	    NSLog(@"Hello, World!");
+	    
+	}
+    return 0;
+}
+
